87a344a8864808d21cabb3468e278eb89e913d64
anishs <anish2sinha@gmail.com>
2024-10-01 13:23:35 -0400

    added ExerciseCard component and incorporated it into Catalog page

---
+++ app/src/components/Card.js
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
+import { Link } from 'react-router-dom';
+
+function ExerciseCard({ image, title, description, link }) {
+    return (
+        <Card sx={{ minWidth: 200, maxWidth: 345 }}>
+            <CardActionArea component={link ? Link : 'div'} to={link ? link : null}>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={image}
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {title}
+                    </Typography>
+                    <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        {description}
+                    </Typography>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+}
+
+export default ExerciseCard;
--- app/src/pages/Catalog.js
+++ app/src/pages/Catalog.js
@@ -1,8 +1,79 @@
+import React from 'react';
+import { Typography, Box } from '@mui/material';
+import ExerciseCard from '../components/Card';
+
 function Catalog() {
     return (
-        <div>
-            <h1>Catalog</h1>
-        </div>
+        <Box sx={{ padding: '20px', textAlign: 'center' }}>
+            <Typography variant="h4" gutterBottom sx={{ marginBottom: '40px' }}>
+                Catalog
+            </Typography>
+
+            <Box
+                sx={{
+                    display: 'grid',
+                    gridTemplateColumns: 'repeat(2, 1fr)',
+                    gap: 8,
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    maxWidth: '720px',
+                    margin: '0 auto',
+                }}
+            >
+                <Box
+                    sx={{
+                        maxWidth: '345px',
+                        margin: 'auto',
+                    }}
+                >
+                    <ExerciseCard
+                        title="Exercise 1"
+                        description="description for exercise 1 description for exercise 1"
+                        link='/'
+                    />
+                </Box>
+
+                <Box
+                    sx={{
+                        maxWidth: '345px',
+                        margin: 'auto',
+                    }}
+                >
+                    <ExerciseCard
+                        title="Exercise 2"
+                        description="description for exercise 2 description for exercise 2"
+                        link='/'
+                    />
+                </Box>
+
+                <Box
+                    sx={{
+                        maxWidth: '345px',
+                        margin: 'auto',
+                    }}
+                >
+                    <ExerciseCard
+                        title="Exercise 3"
+                        description="description for exercise 3 description for exercise 3"
+                        link='/'
+                    />
+                </Box>
+
+                <Box
+                    sx={{
+                        maxWidth: '345px',
+                        margin: 'auto',
+                    }}
+                >
+                    <ExerciseCard
+                        title="Exercise 4"
+                        description="description for exercise 4 description for exercise 4 "
+                        link='/'
+                    />
+                </Box>
+
+            </Box>
+        </Box>
     );
 }
 
