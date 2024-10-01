bef1725c9351bddc8594c37eab17cfa1742235d5
anishs <anish2sinha@gmail.com>
2024-10-01 12:31:39 -0400

    set up router and empty files for pages/components

--- app/src/App.js
+++ app/src/App.js
@@ -1,8 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import About from './pages/About';
+import Menubar from './components/Menubar';
+
 function App() {
   return (
-    <div>
-      <h1>AI Coach</h1>
-    </div>
+    <Router>
+      <div style={{ display: 'flex' }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ app/src/pages/Catalog.js
@@ -0,0 +1,9 @@
+function Catalog() {
+    return (
+        <div>
+            <h1>Catalog</h1>
+        </div>
+    );
+}
+
+export default Catalog;
