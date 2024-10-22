eeb6ed3fa8b93222af82488a3908786c21c9ff4b
Jilin <jilin@bu.edu>
2024-10-22 12:58:23 -0400

    added user greeting upon login, modified child elements in toolbar to consistently use flexGrow property"

--- app/src/components/Menubar.js
+++ app/src/components/Menubar.js
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material';
-import { Link } from 'react-router-dom';
-import { handleLogin, handleLogout } from '../utils/HandleLogin';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import { Link } from "react-router-dom";
+import { handleLogin, handleLogout } from "../utils/HandleLogin";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 /**
  * Menubar is a component that displays a navigation bar with links for all of the main pages.
@@ -13,16 +13,19 @@ import { getAuth, onAuthStateChanged } from 'firebase/auth';
  * @returns {JSX.Element} A Material UI AppBar with navigation links.
  */
 function Menubar() {
-    const [isAuth, setIsAuth] = useState(false);
     const auth = getAuth();
+    const [isAuth, setIsAuth] = useState(false);
+    const [username, setUsername] = useState("");
 
     // call this method whenever authentication changes
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
             if (user) {
                 setIsAuth(true); // signed in
+                setUsername(user.displayName);
             } else {
                 setIsAuth(false); // signed out
+                setUsername(""); // reset the username upon signing out
             }
         });
 
@@ -32,10 +35,10 @@ function Menubar() {
     return (
         <AppBar position="static">
             <Toolbar>
-                <Typography variant="h6" component="div">
+                <Typography variant="h6" component="div" sx={{ userSelect: "none" }}>
                     AI Coach
                 </Typography>
-                <Box sx={{ display: 'flex', gap: 2, ml: 4 }}>
+                <Box sx={{ flexGrow: 1, gap: 2, ml: 4 }}>
                     <Button color="inherit" component={Link} to="/home">
                         Home
                     </Button>
@@ -48,11 +51,16 @@ function Menubar() {
                 </Box>
 
                 <Box sx={{ flexGrow: 1 }} />
+                {isAuth && (
+                    <Typography variant="h6" component="div" sx={{ mr: 4, userSelect: "none" }}>
+                        Welcome {username}!
+                    </Typography>
+                )}
                 <Button color="inherit" onClick={isAuth ? handleLogout : handleLogin}>
-                    {isAuth ? 'Logout' : 'Login'}
+                    {isAuth ? "Logout" : "Login"}
                 </Button>
             </Toolbar>
-        </AppBar >
+        </AppBar>
     );
 }
 
