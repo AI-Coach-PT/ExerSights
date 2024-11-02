1ee34bc5052e2fd085b834c360b0e95848dd5f2a
anishs <anish2sinha@gmail.com>
2024-11-02 14:34:12 -0400

    implemented audio feedback to SquatPage, specifically for Go Down Lower! and Excellent! feedback strings

--- app/src/utils/Audio.js
+++ app/src/utils/Audio.js
@@ -5,9 +5,21 @@ const playSoundCorrectRep = () => {
     audio.play();
 }
 
-const playText = () => {
-    const audio = new Audio(rep);
-    audio.play();
+let selectedVoice = null;
+const setVoice = () => {
+    const voices = window.speechSynthesis.getVoices();
+    selectedVoice = voices.find(voice => voice.name === "Google US English");
+}
+
+window.speechSynthesis.onvoiceschanged = setVoice;
+
+const playText = (text) => {
+    window.speechSynthesis.cancel();
+
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.voice = selectedVoice;
+
+    window.speechSynthesis.speak(utterance);
 }
 
 export { playSoundCorrectRep, playText };
