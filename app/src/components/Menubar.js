20694301f8d60d0ddf084a1938bbbbd2eb509fa4
Jilin <jilin@bu.edu>
2024-11-03 01:18:11 -0400

    made menubar responsive
    resolve conflicts

--- app/src/components/Menubar.js
+++ app/src/components/Menubar.js
@@ -94,72 +94,22 @@ function Menubar() {
                     }}>
                     ExerSights
                 </Typography>
-                {/* Desktop Navigation */}
-                <Box
-                    sx={{
-                        display: { xs: "none", md: "flex" },
-                        flexGrow: { xs: 0, md: 1 },
-                        justifyContent: "left",
-                    }}>
-                    {menuItems.map((item) => (
-                        <Button key={item.text} color="inherit" component={Link} to={item.path}>
-                            {item.text}
-                        </Button>
-                    ))}
-                </Box>
-                {/* Login/Logout Button */}
-                <Box sx={{ display: "flex", justifyContent: "right" }}>
-                    {isAuth && (
-                        <Typography
-                            variant="h6"
-                            component="div"
-                            sx={{ userSelect: "none", display: { sm: "none", md: "flex" } }}>
-                            Welcome {username}!
-                        </Typography>
-                    )}
-                    <Button
-                        color="inherit"
-                        onClick={isAuth ? handleLogout : handleLogin}
-                        sx={{ display: { xs: "none", md: "block" } }}>
-                        {isAuth ? "Logout" : "Login"}
+                <Box sx={{ display: "flex", gap: 2, ml: 4 }}>
+                    <Button color="inherit" component={Link} to="/home">
+                        Home
+                    </Button>
+                    <Button color="inherit" component={Link} to="/catalog">
+                        Catalog
+                    </Button>
+                    <Button color="inherit" component={Link} to="/about">
+                        About
                     </Button>
                 </Box>
-                {/* Drawer Icon */}
-                {showDrawerButton && (
-                    <IconButton color="inherit" onClick={handleDrawerToggle}>
-                        <MenuIcon />
-                    </IconButton>
-                )}
-                {/* Mobile Drawer */}
-                <Drawer
-                    anchor="right"
-                    open={isDrawerOpen}
-                    onClose={handleDrawerToggle}
-                    sx={{
-                        display: { xs: "block", md: "none" },
-                        "& .MuiDrawer-paper": { width: 240 },
-                    }}>
-                    <List>
-                        {menuItems.map((item) => (
-                            <ListItem
-                                key={item.text}
-                                button
-                                component={Link}
-                                to={item.path}
-                                onClick={handleDrawerToggle}>
-                                <ListItemText primary={item.text} />
-                            </ListItem>
-                        ))}
-                        <ListItem
-                            button
-                            onClick={() => {
-                                isAuth ? handleLogout() : handleLogin();
-                                handleDrawerToggle();
-                            }}>
-                            <ListItemText primary={isAuth ? "Logout" : "Login"} />
-                        </ListItem>
-                    </List>
-                </Drawer>
+
+                <Box sx={{ flexGrow: 1 }} />
+                <Button color="inherit" onClick={isAuth ? handleLogout : handleLogin}>
+                    {isAuth ? "Logout" : "Login"}
+                </Button>
             </Toolbar>
         </AppBar>
     );
