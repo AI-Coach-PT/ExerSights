50da1475dee03943986724aca47dd4319c1e8bc2
anishs <anish2sinha@gmail.com>
2024-11-07 09:24:36 -0500

    add audio feedback to pushup exercise, modify pushup exercise to not print 'go down lower' whilst in pushup position

--- app/src/utils/PushUp.js
+++ app/src/utils/PushUp.js
@@ -1,8 +1,10 @@
 import { calculateAngle } from './Angles';
+import { playSoundCorrectRep, playText } from './Audio';
 
 let pushUpCount = 0;
 let inPushUpPosition = false;
 let closerArm = null;
+let lastFeedback = "";
 
 export const checkPushup = (landmarks, onFeedbackUpdate, setCurrElbowAngle, setRepCount, targetElbowAngle = 65) => {
     const thresholdAngle = 150;
@@ -17,7 +19,7 @@ export const checkPushup = (landmarks, onFeedbackUpdate, setCurrElbowAngle, setR
     const leftShoulder = landmarks[11];
     const leftElbow = landmarks[13];
     const leftHand = landmarks[15];
-    
+
     //right arm
     const rightShoulder = landmarks[12];
     const rightElbow = landmarks[14];
@@ -30,7 +32,7 @@ export const checkPushup = (landmarks, onFeedbackUpdate, setCurrElbowAngle, setR
 
     let feedback = "Please Begin Rep!";
 
-    if(closerArm === "Left Arm") {
+    if (closerArm === "Left Arm") {
         currentAngle = leftElbowAngle;
     } else {
         currentAngle = rightElbowAngle;
@@ -38,16 +40,17 @@ export const checkPushup = (landmarks, onFeedbackUpdate, setCurrElbowAngle, setR
 
     setCurrElbowAngle(currentAngle);
 
-    if (currentAngle < thresholdAngle && currentAngle > targetElbowAngle){
+    if (currentAngle < thresholdAngle && currentAngle > targetElbowAngle && !inPushUpPosition) {
         feedback = "Go Down Lower!";
-    } else if (currentAngle < targetElbowAngle ){
+    } else if (currentAngle < targetElbowAngle) {
         feedback = "Excellent!"
         inPushUpPosition = true;
-    } else if (currentAngle > thresholdAngle){
+    } else if (currentAngle > thresholdAngle) {
         if (inPushUpPosition) {
             feedback = "Excellent!"
             pushUpCount++;
             inPushUpPosition = false;
+            playSoundCorrectRep();
             setRepCount(pushUpCount);
         }
     } else {
@@ -56,6 +59,13 @@ export const checkPushup = (landmarks, onFeedbackUpdate, setCurrElbowAngle, setR
         }
     }
 
+    // only play feedback audio from begin -> go down lower and from lower -> excellent
+    if ((feedback === "Go Down Lower!" && lastFeedback === "Please Begin Rep!")
+        || (feedback === "Excellent!" && lastFeedback === "Go Down Lower!")) {
+        playText(feedback);
+    }
+
+    lastFeedback = feedback;
     onFeedbackUpdate(feedback);
 };
 
@@ -71,19 +81,19 @@ export const setPushUpCount = (val) => {
 
 function getCloserArm(landmarks) {
     if (!landmarks) return null;
-  
+
     // Extract z-values for left arm landmarks
     const leftWristZ = landmarks[11].z;
     const leftElbowZ = landmarks[13].z;
     const leftShoulderZ = landmarks[15].z;
     const leftArmAvgZ = (leftWristZ + leftElbowZ + leftShoulderZ) / 3;
-  
+
     // Extract z-values for right arm landmarks
     const rightWristZ = landmarks[12].z;
     const rightElbowZ = landmarks[14].z;
     const rightShoulderZ = landmarks[16].z;
     const rightArmAvgZ = (rightWristZ + rightElbowZ + rightShoulderZ) / 3;
-  
+
     // Determine which arm is closer based on average z-value
     return leftArmAvgZ < rightArmAvgZ ? "Left Arm" : "Right Arm";
-  }
+}
