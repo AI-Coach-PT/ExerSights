918dbd9cd8244d9928504c5bc1b33066b10b756b
James Knee <jknee@bu.edu>
2025-03-02 15:26:51 -0500

    rolled pose detector back

--- app/src/utils/models/PoseDetector.js
+++ app/src/utils/models/PoseDetector.js
@@ -2,7 +2,10 @@ let detectPose;
 
 export default async function getDetectPose(...args) {
     if (!detectPose) {
-        detectPose = (await import('./PoseDetectorTasksVision')).default;
+        detectPose = (await (process.env.REACT_APP_MODEL === 'tasks-vision'
+            ? import('./PoseDetectorTasksVision')
+            : import('./PoseDetectorPose'))
+        ).default;
     }
     return detectPose(...args);
 }
